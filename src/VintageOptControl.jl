2474b5a2dc15ce02c13df9222be2e591c29d4e33
Michael Freiberger <freiberger@iiasa.ac.at>
2023-01-17 12:59:25 +0100

    Base version of the algorithm
    Everything should work for now.

--- src/VintageOptControl.jl
+++ src/VintageOptControl.jl
@@ -1,5 +1,31 @@
 module VintageOptControl
 
-# Write your package code here.
+    using LinearAlgebra
+    using ForwardDiff
+    using Interpolations
+
+    using Plots
+    using ColorSchemes
+    using LaTeXStrings
+    using Printf
+
+    using Serialization
+    using Dierckx
+    using BenchmarkTools
+    using Profile
+    using Coverage
+
+    include("AuxiliaryFunction.jl")
+    include("LineSearch.jl")
+    include("MainFunction.jl")
+    include("ModelFunctions.jl")
+    include("ParametersVariablesSettings.jl")
+    include("ResultsHandling.jl")
+    include("StateSolvers.jl")
+
+
+    export LineSearch
+    export VintageOptimisation
+    export PlotResults
 
 end
